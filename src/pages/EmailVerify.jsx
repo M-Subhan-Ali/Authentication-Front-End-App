d0403ffdc9f2bdb209e925a4012f4ba1e466f434
M-Subhan-Ali <s8289552@gmail.com>
2025-04-20 03:34:49 +0500

    verification Email Update

--- src/pages/EmailVerify.jsx
+++ src/pages/EmailVerify.jsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import { appContext } from "../context/appContext";
+import { toast } from "react-toastify";
+
+
 
 const EmailVerify = () => {
   const navigate = useNavigate();
+
+  const { Backend_URL } = useContext(appContext)
+
+  const SubmitHandler = async () => {
+    try {
+    
+      // axios.defaults.withCredentials = true
+    const { data } = await axios.post(`${Backend_URL}/api/auth/verifyAccount`)
+  
+    if( data.success ){
+     toast.success(data.message)
+     navigate("/")
+    }else{
+      toast.error(data.message)
+    }
+
+
+    
+  } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const inputRefs = useRef([]);
+
+  const HandleOnInput = ( e , index ) => { //hehe for moving to the next input by typing otp hehehe
+  
+    if( e.target.value.length > 0 && inputRefs.current.length - 1 ){
+
+      inputRefs.current[ index + 1 ].focus() ; 
+    
+    }
+  }
+
+
+  const HandleKeyDown = ( e , index ) => {
+    
+    if( e.key === "Backspace" && index > 0 && e.target.value === "" ){
+
+      inputRefs.current[ index - 1].focus();
+    
+    }
+  }
+
+
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400 ">
       <img
@@ -13,7 +64,8 @@ const EmailVerify = () => {
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32
             cursor-pointer"
       />
-      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+      <form onSubmit={SubmitHandler} 
+      className="bg-slate-900 p-8 rounded-lg shadow-lg w-72 md:w-96 text-sm">
       <h1 className="text-white text-2xl font-semibold text-center mb-4">
         Email Verify OTP
       </h1>
@@ -21,14 +73,22 @@ const EmailVerify = () => {
       >
         Enter the 6-digit code send to your email id.
       </p>
-      <div className="flex justify-center mb-8 gap-2">
+      <div className="flex justify-center mb-8 gap-1.5 md:gap-2">
       {
         Array(6).fill(0).map((_,index)=>{
-         return <input type="text" maxLength="1" key={index} required className="
-          w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"/>
+         return <input type="text" maxLength="1" key={index} required 
+         ref={ e => inputRefs.current[index] = e }
+         onInput={ e => HandleOnInput( e , index ) } //for upgrading to next input broo
+         onKeyDown={ e => HandleKeyDown( e , index)} //for going backl to previous input bro
+         className="
+          w-10 md:w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"/>
         })
       }
       </div>
+      <button 
+      type="submit" 
+      className="w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900
+      text-white rounded-full" >Verify Email</button>
       </form>
     </div>
   );
